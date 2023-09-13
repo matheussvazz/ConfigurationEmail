cc70252211d9d6053e42e6a8a69ebf8f27872dcb
Matheus Vaz <matheusvaz1998@hotmail.com>
2023-09-13 08:58:29 -0300

    Lendo do AppSettings

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,6 +1,54 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Text;
+using Blog;
+using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+   x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+   x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+   x.TokenValidationParameters = new TokenValidationParameters
+   {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(key),
+      ValidateIssuer = false,
+      ValidateAudience = false
+   };
+}
+);
+
+builder   // Configura o comportamento da API
+.Services
+.AddControllers()
+.ConfigureApiBehaviorOptions(options =>
+{
+   options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
+
+var app = builder.Build();
+Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+Configuration.ApiKey = app.Configuration.GetValue<string>("Apikey");
+
+var smtp = new Configuration.SmtpConfiguration();
+app.Configuration.GetSection("Smtp").Bind(smtp);
+Configuration.Smtp = smtp;
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
